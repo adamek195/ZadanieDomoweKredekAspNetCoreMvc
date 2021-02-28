22a35f6c8df1959e4c958f221c8510efe75a6656
Adam Bednorz <248955@student.pwr.edu.pl>
2021-02-28 18:49:17 +0100

    dodano zegar do menu

---
+++ AdamBednorzZadanieDomowe6/AdamBednorzZadanieDomowe6/wwwroot/js/scriptClock.js
@@ -0,0 +1,18 @@
+﻿//funkcja pobierajaca aktulany czas i wykorzystujaca go jako zegar
+function CountingTime() {
+    let today = new Date();
+
+    let hour = today.getHours();
+    if (hour < 10)
+        hour = "0" + hour;
+    let minute = today.getMinutes();
+    if (minute < 10)
+        minute = "0" + minute;
+    let second = today.getSeconds();
+    if (second < 10)
+        second = "0" + second;
+
+    document.getElementById("clock").innerHTML = hour + ":" + minute + ":" + second;
+
+    setTimeout("CountingTime()", 1000);
+}
